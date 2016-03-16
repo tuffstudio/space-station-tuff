a84a8d231a42b67fa2b394e6fec16b002342c112
Michał Skóra <michal.skora@chilid.pl>
2016-03-16 10:40:21 +0100

    Case study post type

--- functions.php
+++ functions.php
@@ -10,13 +10,14 @@
  * @link https://github.com/roots/sage/pull/1042
  */
 $sage_includes = [
-  'lib/assets.php',    // Scripts and stylesheets
-  'lib/extras.php',    // Custom functions
-  'lib/setup.php',     // Theme setup
-  'lib/titles.php',    // Page titles
-  'lib/wrapper.php',   // Theme wrapper class
+  'lib/assets.php',     // Scripts and stylesheets
+  'lib/extras.php',     // Custom functions
+  'lib/setup.php',      // Theme setup
+  'lib/titles.php',     // Page titles
+  'lib/wrapper.php',    // Theme wrapper class
   'lib/customizer.php', // Theme customizer
-  'lib/cleaning.php'   // Cleanings
+  'lib/cleaning.php',   // Cleanings
+  'lib/post-types.php'  // Custom post types
 ];
 
 foreach ($sage_includes as $file) {
