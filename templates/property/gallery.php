d3e7b92f255689899c49b17614293eeaff618794
Michał Skóra <michal.skora@chilid.pl>
2016-04-07 10:43:42 +0200

    Code review

--- templates/property/gallery.php
+++ templates/property/gallery.php
@@ -1,6 +1,6 @@
 <section id="gallery" class="property__gallery property__panel js-property-panel visible">
     <div class="property__gallery-slider">
-        <a class="property__gallery-switcher up" href="#" id="prev"></a>
+        <a id="prev" class="property__gallery-switcher up" href="#"></a>
         <div class="cycle-slideshow"
             data-cycle-fx=carousel
             data-cycle-timeout=0
@@ -10,15 +10,15 @@
             data-cycle-carousel-vertical=true
             data-cycle-log=false
         >
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/archi_small.jpg" alt="">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/design_small.jpg" alt="">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/art_small.jpg" alt="">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/design_small.jpg" alt="">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/rent_small.jpg" alt="">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/archi_small.jpg" alt="">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/dist/images/rent_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/archi_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/design_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/art_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/design_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/rent_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/archi_small.jpg" alt="">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/images/rent_small.jpg" alt="">
         </div>
-        <a class="property__gallery-switcher down" href="#" id="next"></a>
+        <a id="next" class="property__gallery-switcher down" href="#"></a>
     </div>
 
     <img class="property__gallery-big-image" src="<?php echo get_template_directory_uri(); ?>/dist/images/banner-placeholder.jpg" alt="">
