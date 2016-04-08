2b96fa8b0caa98220066be72263206df69bd10fb
Michał Skóra <michal.skora@chilid.pl>
2016-04-08 10:28:17 +0200

    Canvas header

---
+++ templates/canvas/header.php
@@ -0,0 +1,9 @@
+<section class="section section--first canvas__header">
+    <div class="container">
+        <div class="grid grid--full">
+            <div class="grid__item desktop--three-quarters push--desktop--one-third">
+                <img class="canvas__title-img" src="<?php echo get_template_directory_uri(); ?>/dist/images/canvas-headline.png" alt="" />
+            </div>
+        </div>
+    </div>
+</section>
--- templates/page-canvas.php
+++ templates/page-canvas.php
@@ -1,12 +1,5 @@
-<section class="section section--first canvas__header">
-    <div class="container">
-        <div class="grid grid--full">
-            <div class="grid__item desktop--three-quarters push--desktop--one-third">
-                <img class="canvas__title-img" src="<?php echo get_template_directory_uri(); ?>/dist/images/canvas-headline.png" alt="" />
-            </div>
-        </div>
-    </div>
-</section>
+<?php include 'canvas/header.php'; ?>
+
 <section class="section">
     <div class="container container--mobile-full js-canvas">
         <div class="grid grid--full">
