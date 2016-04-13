1e129af523a656293c66883dde4fe815b735bfcf
Michał Skóra <michal.skora@chilid.pl>
2016-04-13 16:08:03 +0200

    Init page template

---
+++ templates/page-casestudy.php
@@ -0,0 +1,203 @@
+<section class="section--top-small-space container container--space">
+    <h2 class="title--main">
+        <?php the_title(); ?>
+    </h2>
+    <h1 class="headline--main">
+        Client stories
+    </h1>
+    <article class="text--description">
+
+    </article>
+</section>
+
+<section>
+    <div class="container container--full">
+        <div class="grid grid--full">
+            <div class="grid__item tablet--one-half">
+                <div class="grid__item">
+                    <div class="grid__item tablet--one-half phone--hide tablet--show">
+                        <div class="masonry__item masonry__item--square"></div>
+                    </div><!--
+                    --><div class="grid__item tablet--one-half">
+                        <a href="#" class="masonry__link">
+                            <div class="masonry__item masonry__item--square">
+                                <div class="masonry__tile masonry__tile--white masonry__tile-arrow">
+                                    <div class="masonry__tile-border"></div>
+                                    <div class="masonry__tile-info">
+                                        <p class="masonry__tile-category">Case studies: <span>Commercial</span></p>
+                                        <h3 class="masonry__tile-title">Metropolitan Wharf, e1</h3>
+                                        <p class="masonry__tile-price">2,700pw</p>
+                                        <p class="masonry__tile-desc masonry__tile-desc--big">2 bedroom penthouse</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid__item">
+                    <a href="#" class="masonry__link">
+                        <div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square">
+                                <div class="masonry__tile masonry__tile--white masonry__tile-arrow">
+                                    <div class="masonry__tile-border"></div>
+                                    <div class="masonry__tile-info">
+                                        <p class="masonry__tile-category">Case studies: <span>Residential</span></p>
+                                        <h3 class="masonry__tile-title">Stratford High Street, e15</h3>
+                                        <p class="masonry__tile-price">114,905 pa</p>
+                                        <p class="masonry__tile-desc masonry__tile-desc--big">610sqm UNIT</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!--
+                        --><div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square masonry__image">
+                                <img src="<?= get_template_directory_uri(); ?>/dist/images/masonry_small_placeholder.jpg" alt="">
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div><!--
+            --><div class="grid__item tablet--one-half">
+                <div class="grid__item masonry__image">
+                    <a href="#" class="masonry__link">
+                        <img src="<?= get_template_directory_uri() ?>/dist/images/masonry_big_placeholder.jpg" alt="" class="masonry__background-image">
+                        <div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square"></div>
+                        </div><!--
+                        --><div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square">
+                                <div class="masonry__tile masonry__tile--white">
+                                    <div class="masonry__tile-border"></div>
+                                    <div class="masonry__tile-info">
+                                        <p class="masonry__tile-category">Case studies: <span>Commercial</span></p>
+                                        <h3 class="masonry__tile-title">Camden road, e7</h3>
+                                        <p class="masonry__tile-price">4,350,000</p>
+                                        <p class="masonry__tile-desc masonry__tile-desc--big">5 bedroom penthouse</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!--
+                        --><div class="grid__item">
+                            <div class="masonry__item masonry__item--rectangular"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid--full is-hidden js-section-reveal">
+            <div class="grid__item tablet--one-half">
+                <div class="grid__item">
+                    <div class="grid__item tablet--one-half phone--hide tablet--show">
+                        <div class="masonry__item masonry__item--square"></div>
+                    </div><!--
+                    --><div class="grid__item tablet--one-half">
+                        <a href="#" class="masonry__link">
+                            <div class="masonry__item masonry__item--square">
+                                <div class="masonry__tile masonry__tile--white masonry__tile-arrow">
+                                    <div class="masonry__tile-border"></div>
+                                    <div class="masonry__tile-info">
+                                        <p class="masonry__tile-category">Case studies: <span>Commercial</span></p>
+                                        <h3 class="masonry__tile-title">Metropolitan Wharf, e1</h3>
+                                        <p class="masonry__tile-price">2,700pw</p>
+                                        <p class="masonry__tile-desc masonry__tile-desc--big">2 bedroom penthouse</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="grid__item">
+                    <a href="#" class="masonry__link">
+                        <div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square">
+                                <div class="masonry__tile masonry__tile--white masonry__tile-arrow">
+                                    <div class="masonry__tile-border"></div>
+                                    <div class="masonry__tile-info">
+                                        <p class="masonry__tile-category">Case studies: <span>Residential</span></p>
+                                        <h3 class="masonry__tile-title">Stratford High Street, e15</h3>
+                                        <p class="masonry__tile-price">114,905 pa</p>
+                                        <p class="masonry__tile-desc masonry__tile-desc--big">610sqm UNIT</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!--
+                        --><div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square masonry__image">
+                                <img src="<?= get_template_directory_uri(); ?>/dist/images/masonry_small_placeholder.jpg" alt="">
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div><!--
+            --><div class="grid__item tablet--one-half">
+                <div class="grid__item tablet--one-half">
+                    <div class="masonry__item masonry__item--square">
+                        <div class="masonry__tile masonry__tile-link masonry__tile-link--quote">
+                            <span class="quote">Russel is the master of mortar, the king of concrete and the master of the deal</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid__item masonry__image">
+                    <a href="#" class="masonry__link">
+                        <img src="<?= get_template_directory_uri() ?>/dist/images/masonry_big_placeholder.jpg" alt="" class="masonry__background-image">
+                        <div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square"></div>
+                        </div><!--
+                        --><div class="grid__item one-half">
+                            <div class="masonry__item masonry__item--square">
+                                <div class="masonry__tile masonry__tile--white">
+                                    <div class="masonry__tile-border"></div>
+                                    <div class="masonry__tile-info">
+                                        <p class="masonry__tile-category">Case studies: <span>Commercial</span></p>
+                                        <h3 class="masonry__tile-title">Camden road, e7</h3>
+                                        <p class="masonry__tile-price">4,350,000</p>
+                                        <p class="masonry__tile-desc masonry__tile-desc--big">5 bedroom penthouse</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div><!--
+                        --><div class="grid__item">
+                            <div class="masonry__item masonry__item--rectangular"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid--full is-hidden js-section-reveal">
+            <div class="grid__item tablet--one-half">
+                <a href="#" class="masonry__link">
+                    <div class="grid__item one-half">
+                        <div class="masonry__item masonry__item--square">
+                            <div class="masonry__tile masonry__tile--white masonry__tile-arrow">
+                                <div class="masonry__tile-border"></div>
+                                <div class="masonry__tile-info">
+                                    <p class="masonry__tile-category">Case studies: <span>Commercial</span></p>
+                                    <h3 class="masonry__tile-title">Stratford High Street, e15</h3>
+                                    <p class="masonry__tile-price">114,905 pa</p>
+                                    <p class="masonry__tile-desc masonry__tile-desc--big">610sqm UNIT</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--
+                    --><div class="grid__item one-half">
+                        <div class="masonry__item masonry__item--square masonry__image">
+                            <img src="<?= get_template_directory_uri(); ?>/dist/images/masonry_small_placeholder.jpg" alt="">
+                        </div>
+                    </div>
+                </a>
+            </div><!--
+            --><div class="grid__item tablet--one-half">
+                <div class="grid__item tablet--one-half phone--hide tablet--show"></div><!--
+                --><div class="grid__item tablet--one-half">
+                    <div class="masonry__item masonry__item--square">
+                        <div class="masonry__tile masonry__tile-link masonry__tile-link--quote">
+                            <span class="quote">Russel is the master of mortar, the king of concrete and the master of the deal</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'homepage/stay-in-touch.php'; ?>
