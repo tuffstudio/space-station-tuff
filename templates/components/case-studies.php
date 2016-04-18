908ea4a63002e194b899932cee679509468bf4e7
Michał Skóra <michal.skora@chilid.pl>
2016-04-18 09:47:53 +0200

    Homepage case studies refactoring

--- templates/components/case-studies.php
+++ templates/components/case-studies.php
@@ -1,5 +1,5 @@
 <?php
-    use Roots\Sage\ExcerptText;
+    use Roots\Sage\MagazinePost;
 ?>
 
 <section class="section case-studies is-hidden js-section-reveal <?= is_front_page() ? '' : 'section--small-space';?>">
@@ -8,54 +8,34 @@
             <?php
             $case_studies = CFS() -> get('ss_case_studies_list', $post->ID);
 
-            $args_case_study = array(
-                'post_type' => 'case-study',
-                'post_status' => 'publish',
-                'post__in' => $case_studies,
-            );
-
-            $query = new WP_Query($args_case_study);
-
-            if ( $query->have_posts() ) :
-                while ( $query->have_posts() ) :
-                    $query->the_post();
-                    $case_study_id = $post->ID;
-                    $case_study_category = get_the_terms($case_study_id, 'case_study_category')[0]->name;
-                    $case_study_link = get_permalink($case_study_id);
-
-                    $case_study_fields = CFS()->get(false, $case_study_id, array( 'format' => 'raw' ));
-                    $case_study_img_id = $case_study_fields['case_study_thumbnail'];
-                    $case_study_img_url = get_the_post_thumbnail($case_study_id, 'case_study');
-                    $case_study_video = $case_study_fields['case_study_video_link'];
+            if ( $case_studies ) :
+                foreach ($case_studies as $key => $id) :
+                    $case_study = new MagazinePost\CaseStudyPost($id);
             ?>
                 <div class="grid grid--middle">
                     <div class="grid__item tablet--one-half">
                         <div class="case-study__info">
-                            <p class="section__category">Case studies: <span><?= $case_study_category; ?></span></p>
+                            <p class="section__category">Case studies: <span><?= $case_study->get_category(); ?></span></p>
                             <h2 class="headline--default">
-                                <a href="<?= $case_study_link; ?>">
-                                    <?= the_title(); ?>
+                                <a href="<?= $case_study->get_link(); ?>">
+                                    <?= $case_study->get_title(); ?>
                                 </a>
                             </h2>
                             <div class="case-study__description">
-                                <?php
-                                    $description = $post->post_content;
-
-                                    echo ExcerptText\getShortText($description, 300);
-                                ?>
+                                <?= $case_study->get_excerpt(300); ?>
                             </div>
 
-                            <a href="<?= $case_study_link; ?>" class="btn btn--primary">View case study</a>
+                            <a href="<?= $case_study->get_link(); ?>" class="btn btn--primary">View case study</a>
                         </div>
                     </div><!--
                     --><div class="grid__item tablet--one-half phone--hide tablet--show">
-                        <div class="case-study__thumbnail <?= strlen($case_study_video) != 0 ? 'is-video' : ''?>">
-                            <?= $case_study_img_url; ?>
+                        <div class="case-study__thumbnail <?= $case_study->has_video() ? 'is-video' : ''?>">
+                            <?= $case_study->get_image('case_study'); ?>
                             <button class="btn btn--play">play</button>
                         </div>
                     </div>
                 </div>
-            <?php endwhile; endif; wp_reset_postdata(); ?>
+            <?php endforeach; endif; ?>
         </div>
     </div>
 </section>
