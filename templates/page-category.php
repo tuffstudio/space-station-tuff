18a9744aa129b17682ed46eff6fc1139479618b0
Michał Skóra <michal.skora@chilid.pl>
2016-04-18 12:11:11 +0200

    Category page

---
+++ templates/page-category.php
@@ -0,0 +1,56 @@
+<?php use Roots\Sage\MagazinePost; ?>
+<?php include 'canvas/header.php'; ?>
+
+<section class="section section--bottom-stains">
+    <div class="container js-canvas">
+        <div class="grid grid--full">
+            <div class="grid__item desktop--one-quarter js-target">
+                <?php include 'canvas/navigation.php' ?>
+            </div><!--
+            --><div class="grid__item desktop--three-quarters js-owner" style="overflow-x: hidden;">
+                <?php
+                    $i = 1;
+                    while (have_posts()) :
+                        the_post();
+                        $canvas_post = new MagazinePost\MagazinePost($post->ID);
+                ?>
+                    <div class="single-canvas">
+                        <div class="grid grid--middle <?= $i % 2 == 0 ? 'grid--rev' : ''; ?>">
+                            <div class="grid__item tablet--one-third desktop--two-fifths single-canvas__half">
+                                <a href="<?= $canvas_post->get_link(); ?>" class="masonry__link">
+                                    <div class="single-canvas__thumbnail">
+                                        <?= $canvas_post->get_image('medium'); ?>
+                                    </div>
+                                </a>
+                            </div><!--
+                            --><div class="grid__item tablet--two-thirds desktop--three-fifths single-canvas__half">
+                                <div class="single-canvas__content">
+                                    <p class="masonry__tile-category">Canvas <span><?= $canvas_post->get_category(); ?></span></p>
+                                    <a href="<?= $canvas_post->get_link(); ?>" class="single-canvas__link">
+                                        <h2 class="canvas-post__title canvas-post__title--small"><?= $canvas_post->get_title(); ?></h2>
+                                    </a>
+                                    <div class="text-description"><?= $canvas_post->get_excerpt(120); ?>...</div>
+                                    <p class="canvas-post__info canvas-post__info--space">Posted by <?= get_the_author(); ?>, <?= get_the_date('l j F Y'); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php $i++; endwhile;  ?>
+
+                <div class="pagination">
+                    <div class="grid grid--full">
+                        <div class="grid__item one-half u--align-left">
+                            <?php previous_posts_link(); ?>
+                        </div><!--
+                        --><div class="grid__item one-half u--align-right">
+                            <?php next_posts_link(); ?>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'homepage/stay-in-touch.php'; ?>
