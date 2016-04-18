e2778c12a0e457c3f10879890a20f5993ef7634b
sebastian.musial <Sebastian@iMac-sebastianmusial.local>
2016-04-18 10:54:53 +0200

    Canvas nagiation - image animation"

--- templates/canvas/header.php
+++ templates/canvas/header.php
@@ -2,7 +2,9 @@
     <div class="container">
         <div class="grid grid--full">
             <div class="grid__item desktop--three-quarters push--desktop--one-quarter">
-                <img class="canvas__title-img" src="<?php echo get_template_directory_uri(); ?>/dist/images/canvas-headline.png" alt="" />
+                <a href="<?= get_permalink(97); ?>">
+                    <img class="canvas__title-img" src="<?php echo get_template_directory_uri(); ?>/dist/images/canvas-headline.png" alt="" />
+                </a>
             </div>
         </div>
     </div>
