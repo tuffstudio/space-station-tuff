52c4557fee2e3b0c853a2f4c04d831f403d3730a
Michał Skóra <michal.skora@chilid.pl>
2016-04-26 11:44:27 +0200

    Join us section

--- templates/contact/content.php
+++ templates/contact/content.php
@@ -1,6 +1,7 @@
 <?php
     $contact_fields = CFS() -> get(false, $post->ID);
-    $contact_phone_trimmed = str_replace(' ', '', $contact_fields['first_office_phone_number']);
+    $first_contact_phone_trimmed = str_replace(' ', '', $contact_fields['first_office_phone_number']);
+    $second_contact_phone_trimmed = str_replace(' ', '', $contact_fields['second_office_phone_number']);
 ?>
 
 <section class="section contact-section">
@@ -15,7 +16,7 @@
             <div class="grid__item tablet--one-half u--align-center">
                 <div class="contact__office">
                     <h2 class="contact__office-name u--text-uppercase "><?= $contact_fields['first_office_name']; ?></h2>
-                    <a class="contact__office-link contact__office-phone" href="tel:<?= $contact_phone_trimmed; ?>"><?= $contact_fields['first_office_phone_number']; ?></a>
+                    <a class="contact__office-link contact__office-phone" href="tel:<?= $first_contact_phone_trimmed; ?>"><?= $contact_fields['first_office_phone_number']; ?></a>
                     <a class="contact__office-link u--text-uppercase " href="mailto:<?= $contact_fields['first_office_email']; ?>"><?= $contact_fields['first_office_email']; ?></a>
                     <p class="contact__office-address u--text-uppercase "><?= $contact_fields['first_office_address']; ?></p>
                     <a href="#" class="link--italic">View on map</a>
@@ -23,10 +24,10 @@
             </div><!--
             --><div class="grid__item tablet--one-half u--align-center">
                 <div class="contact__office active">
-                    <h2 class="contact__office-name u--text-uppercase "><?= $contact_fields['first_office_name']; ?></h2>
-                    <a class="contact__office-link contact__office-phone" href="tel:<?= $contact_phone_trimmed; ?>"><?= $contact_fields['first_office_phone_number']; ?></a>
-                    <a class="contact__office-link u--text-uppercase" href="mailto:<?= $contact_fields['first_office_email']; ?>"><?= $contact_fields['first_office_email']; ?></a>
-                    <p class="contact__office-address u--text-uppercase"><?= $contact_fields['first_office_address']; ?></p>
+                    <h2 class="contact__office-name u--text-uppercase "><?= $contact_fields['second_office_name']; ?></h2>
+                    <a class="contact__office-link contact__office-phone" href="tel:<?= $second_contact_phone_trimmed; ?>"><?= $contact_fields['second_office_phone_number']; ?></a>
+                    <a class="contact__office-link u--text-uppercase" href="mailto:<?= $contact_fields['second_office_email']; ?>"><?= $contact_fields['second_office_email']; ?></a>
+                    <p class="contact__office-address u--text-uppercase"><?= $contact_fields['second_office_address']; ?></p>
                     <a href="#" class="link--italic">View on map</a>
                 </div>
             </div>
