c7b219510cc67893478a8ff5cb27f87a74d2fe15
Michał Skóra <michal.skora@chilid.pl>
2016-04-26 16:44:29 +0200

    Search page map fix

--- assets/scripts/_search.js
+++ assets/scripts/_search.js
@@ -1,12 +1,27 @@
 window.SS = window.SS || {};
 
 window.SS.search = function($) {
+    var mapOn = false;
+
+    function turnMapOn() {
+        $button = $('.js-map-switch');
+
+        $button.on('click', function(event) {
+            event.preventDefault();
+
+            if(!mapOn) {
+                var map = new window.SS.PropertyMap('map');
+                map.init();
+                map.setupMarkers();
+
+                mapOn = true;
+            }
+        })
+    }
+
     $(document).ready(function() {
         SS.switchGrids('.js-grid-switcher', '.js-results-block');
         SS.switchGrids('.js-tab-switcher', '.js-tab-panel');
-
-        var map = new window.SS.PropertyMap('map');
-        map.init();
-        map.setupMarkers();
+        turnMapOn();
     });
 };
