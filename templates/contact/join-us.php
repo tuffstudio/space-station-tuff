941f0395ffb3e5ca2b10e775175072d8e8ad3a26
Michał Skóra <michal.skora@chilid.pl>
2016-04-29 15:37:05 +0200

    SPACE-865

--- templates/contact/join-us.php
+++ templates/contact/join-us.php
@@ -1,10 +1,10 @@
 <section class="section--free-space join-us">
     <div class="container">
-        <div class="grid grid--full">
-            <div class="grid__item tablet--one-half u--align-center">
-                <h3 class="join-us__title u--text-uppercase">join us</h3>
+        <div class="grid">
+            <div class="grid__item tablet--one-half join-us__left">
+                <h3 class="join-us__title u--text-uppercase">follow us</h3>
             </div><!--
-            --><div class="grid__item tablet--one-half u--align-center">
+            --><div class="grid__item tablet--one-half join-us__right">
 
                 <?php include dirname(__FILE__) . '/../components/socials.php'; ?>
 
