894bed7fb5a7f985faf4249058ccbf03d74ccd63
Michał Skóra <michal.skora@chilid.pl>
2016-04-29 15:46:54 +0200

    SPACE-857

--- templates/contact/join-us.php
+++ templates/contact/join-us.php
@@ -5,9 +5,7 @@
                 <h3 class="join-us__title u--text-uppercase">follow us</h3>
             </div><!--
             --><div class="grid__item tablet--one-half join-us__right">
-
                 <?php include dirname(__FILE__) . '/../components/socials.php'; ?>
-
             </div>
         </div>
     </div>
