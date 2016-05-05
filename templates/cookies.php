e2e740f516f5100a7ec84c817651fa10a544afb8
Michał Skóra <michal.skora@chilid.pl>
2016-05-05 11:31:59 +0200

    Cookies bar settings in wp panel

--- templates/cookies.php
+++ templates/cookies.php
@@ -1,6 +1,10 @@
-<div class="cookies-info js-cookies-bar js-cookies-close">
+<?php
+    $cookies_text = get_option('cookies_text');
+    $cookies_bg = get_option('cookies_bg');
+?>
+<div class="cookies-info js-cookies-bar js-cookies-close" style="background-color: <?= $cookies_bg; ?>;">
     <p class="cookies-info__body u--text-uppercase">
-        This website uses cookies to improve your experience. If you continue we'll assume that your are happy with it
+        <?= $cookies_text; ?>
     </p>
     <i class="icon icon-cancel cookies-info__close js-cookies-close"></i>
 </div>
