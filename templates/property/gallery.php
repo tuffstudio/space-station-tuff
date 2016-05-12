e99689f993d6acf42d3c585f435eb2bf8dd53124
Michał Skóra <michal.skora@chilid.pl>
2016-05-12 15:04:49 +0200

    Image counter mobile && images more responsive

--- templates/property/gallery.php
+++ templates/property/gallery.php
@@ -6,7 +6,7 @@
     <div class="owl-carousel js-property-gallery">
         <?php foreach ($item->media->images->image as $image) : ?>
             <?php if ( empty($image->tags) ) : ?>
-                <img class="owl-lazy property__gallery-big-image" data-src="<?= $image[0]->baseurl . "/1920x768/" . $image[0]->filename; ?>" alt="<?= $image[0]->filename; ?>">
+                <div class="owl-lazy property__gallery-big-image" data-src="<?= $image[0]->baseurl . "/1920x768/" . $image[0]->filename; ?>" alt="<?= $image[0]->filename; ?>"></div>
             <?php endif; ?>
         <?php endforeach; ?>
     </div>
