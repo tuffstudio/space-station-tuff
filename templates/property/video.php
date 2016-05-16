cad73abd41e31e89e7a446fb2dec9d3fc0516232
sebastian.musial <Sebastian@iMac-sebastianmusial.local>
2016-05-16 15:07:02 +0200

    Video on property && bug with favourites box

--- templates/property/video.php
+++ templates/property/video.php
@@ -1,3 +1,15 @@
-<section id="video" class="property__panel js-property-panel">
-    property video
+<?php
+    // Example video url
+    $video_url = "https://s3.amazonaws.com/propertybase-clients/00D24000000d8IIEAY/a0C24000004W90P/p1a0hthjeimpq8j72qq1j4g13rd2/test.iphone4.m4v";
+?>
+
+<section id="video" class="property__panel property__panel--video js-property-panel">
+    <video
+        class="video--box video-js vjs-default-skin"
+        src="<?= $video_url; ?>"
+        controls preload="auto"
+        data-setup="{}"
+    >
+        Your browser does nor support video tag.
+    </video>
 </section>
