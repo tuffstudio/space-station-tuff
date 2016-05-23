476fba7cc8d6444700df39d32eb1ea710c3ab6d7
Michał Skóra <michal.skora@chilid.pl>
2016-05-23 09:51:43 +0200

    Code review

--- templates/property/map.php
+++ templates/property/map.php
@@ -1,4 +1,4 @@
 <section id="map" class="property__panel js-property-panel">
-    <div id="property-map" class="js-map-switch" data-json='' style="height: 50vh;"></div>
+    <div id="property-map" class="property__map js-map-switch" data-json=''></div>
 </section>
 <script src="https://maps.googleapis.com/maps/api/js"></script>
